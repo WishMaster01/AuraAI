82b55f2a60c491f3436b432b209d5c8eee4d7428
WishMaster01 <yadavsumit2589@gmail.com>
2025-07-23 09:10:42 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: { "http://localhost:5173",
+    "https://auraai-lm91.onrender.com"
+            },
     credentials: true,
   })
 );

4b2669b481e8186021c14a5485799a51a5cc0775
WishMaster01 <yadavsumit2589@gmail.com>
2025-07-23 09:02:40 +0530

    Update UserContext.jsx

--- client/context/UserContext.jsx
+++ client/context/UserContext.jsx
@@ -5,7 +5,7 @@ import React, { createContext, useEffect, useState } from "react";
 export const userDataContext = createContext();
 
 function UserContext({ children }) {
-  const serverUrl = "http://localhost:8080";
+  const serverUrl = "https://auraai-server.onrender.com";
 
   const [userData, setUserData] = useState(null);
   const [frontendImage, setFrontendImage] = useState(null);
